5717c65514a32f88850ad982985b0240f3912cc4
AnhVuong146 <99466654+AnhVuong146@users.noreply.github.com>
2022-05-10 17:59:45 -0700

    Add files via upload

---
+++ flask/Flask/app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+my_obj = Flask(__name__)
+
+my_obj.config.from_mapping(SECRET_KEY = 'AnhVuong')
+
+from app import routes
---
+++ flask/Flask/run.py
@@ -0,0 +1,6 @@
+from app import my_obj
+
+if __name__ == "__main__":
+   my_obj.run(debug = True)
+
+my_obj.run()
