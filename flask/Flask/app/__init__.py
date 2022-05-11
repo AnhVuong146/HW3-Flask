45610477992e57538cf875c3156cf87e947e6c74
AnhVuong146 <99466654+AnhVuong146@users.noreply.github.com>
2022-05-10 18:06:36 -0700

    Delete flask directory

--- flask/Flask/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-my_obj = Flask(__name__)
-
-my_obj.config.from_mapping(SECRET_KEY = 'AnhVuong')
-
-from app import routes
--- flask/Flask/run.py
+++
@@ -1,6 +0,0 @@
-from app import my_obj
-
-if __name__ == "__main__":
-   my_obj.run(debug = True)
-
-my_obj.run()
